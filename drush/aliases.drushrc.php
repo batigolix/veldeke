99d3d78f73a2f37429c480f3efb37960b7761713
Boris Doesborg <bdoesborg@gmail.com>
2019-06-11 23:02:03 +0200

    docksal

---
+++ drush/aliases.drushrc.php
@@ -0,0 +1,18 @@
+<?php
+
+// Define the testing environment.
+$aliases['dev'] = array(
+  'root' => '/var/www/html/docroot',
+  'uri' => 'http://veldeke.test',
+  'remote-host' => 'localhost',
+  'remote-user' => 'root',
+);
+
+// Define the testing environment.
+$aliases['dxdev'] = array(
+  'root' => '/var/www/docroot',
+  'uri' => 'http://veldeke.docksal',
+  // 'remote-host' => 'localhost',
+  // 'remote-user' => 'root',
+);
+
